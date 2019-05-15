0534cc79cf3fdc877e3697346a420b492fa0ba39
Matias <matpaezpaz@gmail.com>
2019-05-15 01:38:21 -0300

    Implemented localStorage

--- js/App.js
+++ js/App.js
@@ -7,7 +7,13 @@ const inputBalance = document.getElementById("balance");
 
 const tableClientes = document.getElementById("tableClientes");
 const tableClientesBody = document.createElement('tbody');
+
+var clientesInStorage = localStorage.getItem("clientes");
+clientesInStorage = clientesInStorage?clientesInStorage:"[]";
+
+const CLIENTES = JSON.parse(clientesInStorage).map( cliente => clienteInStorageToInstance(cliente) );
 tableClientes.appendChild(tableClientesBody);
+CLIENTES.forEach( cliente => tableClientesBody.appendChild(crearFila(cliente)) );
 
 
 formCliente.addEventListener('submit', function(event){
@@ -18,6 +24,9 @@ formCliente.addEventListener('submit', function(event){
     let balance  = inputBalance.value;
     let nuevoCliente = new Cliente(nombre,apellido,dni,balance);
     tableClientesBody.appendChild(crearFila(nuevoCliente));
+    CLIENTES.push(nuevoCliente);
+    localStorage.setItem("clientes",JSON.stringify(CLIENTES));
+    formCliente.reset();
 });
 
 function crearFila(cliente){
@@ -38,4 +47,12 @@ function createElementWithTextContent(element, textContent) {
     let newContent = document.createTextNode(textContent);
     newDOMelement.appendChild(newContent);
     return newDOMelement;
+}
+function clienteInStorageToInstance ( object ) {
+    let nombre = object._nombre;
+    let apellido = object._apellido;
+    let dni = object._dni;
+    let balance  = object._balance;
+    let cliente = new Cliente(nombre,apellido,dni,balance);
+    return cliente;
 }
